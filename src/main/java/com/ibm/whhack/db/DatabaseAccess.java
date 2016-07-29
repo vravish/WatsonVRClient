07c993316be3f90ab4347558063f779c12863467
vravish <vravish@us.ibm.com>
2016-07-29 10:55:30 -0400

    Changed pom.xml to remove jetty and added requests table

--- src/main/java/com/ibm/whhack/db/DatabaseAccess.java
+++ src/main/java/com/ibm/whhack/db/DatabaseAccess.java
@@ -1,5 +1,8 @@
 package com.ibm.whhack.db;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,18 +17,27 @@ public class DatabaseAccess {
 	static final String PASS = "rootpass";
 
 	static Connection conn = null;
-	static PreparedStatement searchStmt = null, addStmt = null;
+	static PreparedStatement searchUsersStmt = null, addUserStmt = null, searchRequestsStmt = null, addRequestStmt = null;
 	
 	static {
 		try {
 			Class.forName(JDBC_DRIVER);
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			searchStmt = 	conn.prepareStatement("SELECT * FROM `enterpriseregistrydb`.`users`"
+			
+			searchUsersStmt = 	conn.prepareStatement("SELECT * FROM `enterpriseregistrydb`.`users`"
 												+ "WHERE `username` = ?");
-			addStmt = 	conn.prepareStatement("INSERT INTO `enterpriseregistrydb`.`users`"
+			addUserStmt = 	conn.prepareStatement("INSERT INTO `enterpriseregistrydb`.`users`"
 											+ "(`username`, `password`, `authorized_user`)"
 											+ "VALUES"
 											+ "(?, ?, ?)");
+			
+			searchRequestsStmt = 	conn.prepareStatement("SELECT * FROM `enterpriseregistrydb`.`requests`"
+														+ "WHERE `username` = ?");
+			addRequestStmt = 	conn.prepareStatement("INSERT INTO `enterpriseregistrydb`.`requests`"
+											+ "(`username`, `image_stream`, "
+											+ "`classifier_id`,`request_timestamp`)"
+											+ "VALUES"
+											+ "(?, ?, ?, now())");
 			
 			String createUsersTable = "CREATE TABLE IF NOT EXISTS `enterpriseregistrydb`.`users` ("
 									+ "`username` VARCHAR(45) NOT NULL,"
@@ -34,23 +46,22 @@ public class DatabaseAccess {
 									+ "PRIMARY KEY (`username`))";
 			
 			conn.createStatement().execute(createUsersTable);
-			
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void createUser(String username, String password, boolean authorizedUser) throws SQLException {
-		addStmt.setString(1, username.toLowerCase());
-		addStmt.setString(2, password);
-		addStmt.setBoolean(3, authorizedUser);
-		addStmt.executeUpdate();
+		addUserStmt.setString(1, username.toLowerCase());
+		addUserStmt.setString(2, password);
+		addUserStmt.setBoolean(3, authorizedUser);
+		addUserStmt.executeUpdate();
 	}
 	
 	public static boolean userValid(String username, String password) throws SQLException {
 		username = username.toLowerCase();
-		searchStmt.setString(1, username);
-		ResultSet rs = searchStmt.executeQuery();
+		searchUsersStmt.setString(1, username);
+		ResultSet rs = searchUsersStmt.executeQuery();
 		while (rs.next()) {
 			String passwdFromDb = rs.getString(2);
 			if (passwdFromDb.equals(password))
@@ -61,11 +72,32 @@ public class DatabaseAccess {
 	
 	public static boolean userAuthorized(String username) throws SQLException {
 		username = username.toLowerCase();
-		searchStmt.setString(1, username);
-		ResultSet rs = searchStmt.executeQuery();
+		searchUsersStmt.setString(1, username);
+		ResultSet rs = searchUsersStmt.executeQuery();
 		while (rs.next()) {
 			return rs.getBoolean(3);
 		}
 		return false;
+	}
+	
+	public static void insertRequest(String username, String classid, File file) throws SQLException, FileNotFoundException {
+		FileInputStream io = new FileInputStream(file);
+		addRequestStmt.setString(1, username.toLowerCase());
+		addRequestStmt.setBinaryStream(2, io , (int)file.length());
+		addRequestStmt.setString(3, classid);
+		addRequestStmt.executeUpdate();
+	}
+	
+	public static void retrieve(String username) throws SQLException {
+		username = username.toLowerCase();
+		searchRequestsStmt.setString(1, username);
+		ResultSet rs = searchRequestsStmt.executeQuery();
+		while (rs.next()) {
+			System.out.println(rs.getString(1));
+		//	String passwdFromDb = rs.getString(2);
+//			if (passwdFromDb.equals(password))
+//				return true;
+		}
+//		return false;
 	}
 }
--- src/main/java/com/ibm/whhack/vrapi/WatsonVRClient.java
+++ src/main/java/com/ibm/whhack/vrapi/WatsonVRClient.java
@@ -30,52 +30,65 @@ public class WatsonVRClient {
 	ClassifierProperties props;
 	ObjectMapper mapper;
 	HttpClient client;
-	
+
 	public WatsonVRClient() throws Exception {
 		mapper = new ObjectMapper();
-		
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
-                false);
-		
+
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
 		creds = mapper.readValue(new File("watson_credentials.json"), APICredentials.class);
 		props = mapper.readValue(new File("classifier_properties.json"), ClassifierProperties.class);
-		
+
 		client = HttpClientBuilder.create().build();
 	}
-	
+
 	public String getBaseAPI() {
 		return creds.getCredentials().getUrl();
 	}
-	
+
 	public String getKey() {
 		return creds.getCredentials().getApi_key();
 	}
-	
+
 	/**
 	 * Sends a request to the endpoint /v3/classifiers to create a classifier.
-	 * Refer to <link>https://watson-api-explorer.mybluemix.net/apis/visual-recognition-v3</link> for more information.  
-	 * @param negativeExamples (FORM) ZIP data containing images that don't match
-	 * @param classifierName (FORM) name of the new classifier created
-	 * @param version (QUERY) release date of the API you want (e.g. 2016-05-14)
-	 * @param positiveExampleClassnamesAndZips (FORM) each class you would like to create should be represented by an array like:
-	 * [className, zipDataContainingPositiveExampleImages]
-	 * @throws URISyntaxException from URI builder
-	 * @throws ExecutionException from Jetty REST call
-	 * @throws TimeoutException from Jetty REST call
-	 * @throws InterruptedException from Jetty REST call
-	 * @throws IOException from Jackson parser
-	 * @throws JsonMappingException from Jackson parser
-	 * @throws JsonParseException from Jackson parser
+	 * Refer to https://watson-api-explorer.mybluemix.net/apis/visual-recognition-v3 for more information.
+	 * 
+	 * @param negativeExamples
+	 *            (FORM) ZIP data containing images that don't match
+	 * @param classifierName
+	 *            (FORM) name of the new classifier created
+	 * @param version
+	 *            (QUERY) release date of the API you want (e.g. 2016-05-14)
+	 * @param positiveExampleClassnamesAndZips
+	 *            (FORM) each class you would like to create should be
+	 *            represented by an array like: [className,
+	 *            zipDataContainingPositiveExampleImages]
+	 * @throws URISyntaxException
+	 *             from URI builder
+	 * @throws ExecutionException
+	 *             from Jetty REST call
+	 * @throws TimeoutException
+	 *             from Jetty REST call
+	 * @throws InterruptedException
+	 *             from Jetty REST call
+	 * @throws IOException
+	 *             from Jackson parser
+	 * @throws JsonMappingException
+	 *             from Jackson parser
+	 * @throws JsonParseException
+	 *             from Jackson parser
 	 */
-	public HashMap<String, Object> createClassifier(String negativeExamples, String classifierName, String version, String[]... positiveExampleClassnamesAndZips) throws URISyntaxException, InterruptedException, TimeoutException, ExecutionException, JsonParseException, JsonMappingException, IOException {
+	public HashMap<String, Object> createClassifier(String negativeExamples, String classifierName, String version,
+			String[]... positiveExampleClassnamesAndZips) throws URISyntaxException, InterruptedException,
+			TimeoutException, ExecutionException, JsonParseException, JsonMappingException, IOException {
 		URIBuilder builder = new URIBuilder();
-		builder.setScheme("https").setHost(getBaseAPI().replace("https://","")).setPath("/v3/classifiers")
-			.setParameter("api_key", getKey())
-			.setParameter("version", version);
+		builder.setScheme("https").setHost(getBaseAPI().replace("https://", "")).setPath("/v3/classifiers")
+				.setParameter("api_key", getKey()).setParameter("version", version);
 		URI uri = builder.build();
-		
+
 		HttpPost post = new HttpPost(uri);
-		
+
 		MultipartEntityBuilder me = MultipartEntityBuilder.create();
 		if (negativeExamples != null)
 			me.addBinaryBody("negative_examples", negativeExamples.getBytes());
@@ -84,83 +97,98 @@ public class WatsonVRClient {
 			me.addBinaryBody(singleClass[0] + "_positive_examples", singleClass[1].getBytes());
 		}
 		post.setEntity(me.build());
-		
+
 		HttpResponse response = client.execute(post);
-		
+
 		if (response.getStatusLine().getStatusCode() != 200)
-			throw new RuntimeException("The service was unsuccessful in processing the request! HTTP: " + response.getStatusLine());
-		
-		BufferedReader rd = new BufferedReader(
-		        new InputStreamReader(response.getEntity().getContent()));
+			throw new RuntimeException(
+					"The service was unsuccessful in processing the request! HTTP: " + response.getStatusLine());
+
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
 		StringBuffer result = new StringBuffer();
 		String line = "";
 		while ((line = rd.readLine()) != null) {
 			result.append(line);
 		}
-		
-//		Fields fields = new Fields();
-//		if (negativeExamples != null)
-//			fields.put("negative_examples", negativeExamples);
-//		fields.put("name", classifierName);
-//		for (String[] singleClass : positiveExampleClassnamesAndZips) {
-//			fields.put(singleClass[0] + "_positive_examples", singleClass[1]);
-//		}
-//		
-//		ContentResponse response = client.FORM(uri, fields);
-//		if (response.getStatus() != 200)
-//			throw new RuntimeException("The service was unsuccessful in processing the request! HTTP: " + response.getStatus());
-		
-		TypeReference<HashMap<String,Object>> typeRef = new TypeReference<HashMap<String,Object>>() {};
 
-	    return mapper.readValue(result.toString(), typeRef);
+		// Fields fields = new Fields();
+		// if (negativeExamples != null)
+		// fields.put("negative_examples", negativeExamples);
+		// fields.put("name", classifierName);
+		// for (String[] singleClass : positiveExampleClassnamesAndZips) {
+		// fields.put(singleClass[0] + "_positive_examples", singleClass[1]);
+		// }
+		//
+		// ContentResponse response = client.FORM(uri, fields);
+		// if (response.getStatus() != 200)
+		// throw new RuntimeException("The service was unsuccessful in
+		// processing the request! HTTP: " + response.getStatus());
+
+		TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {
+		};
+
+		return mapper.readValue(result.toString(), typeRef);
 	}
-	
+
 	/**
-	 * Sends a request to the endpoint /v3/classifiers/{classifier_id} to check status of a classifier.
-	 * Refer to <link>https://watson-api-explorer.mybluemix.net/apis/visual-recognition-v3</link> for more information.  
-	 * @param classifierID name of the new classifier created
-	 * @param version release date of the API you want (e.g. 2016-05-14)
-	 * @throws IOException from Jackson parser
-	 * @throws JsonMappingException from Jackson parser
-	 * @throws JsonParseException from Jackson parser
-	 * @throws TimeoutException 
-	 * @throws ExecutionException 
-	 * @throws InterruptedException 
-	 * @throws URISyntaxException 
+	 * Sends a request to the endpoint /v3/classifiers/{classifier_id} to check
+	 * status of a classifier. Refer to
+	 * <link>https://watson-api-explorer.mybluemix.net/apis/visual-recognition-
+	 * v3</link> for more information.
+	 * 
+	 * @param classifierID
+	 *            name of the new classifier created
+	 * @param version
+	 *            release date of the API you want (e.g. 2016-05-14)
+	 * @throws IOException
+	 *             from Jackson parser
+	 * @throws JsonMappingException
+	 *             from Jackson parser
+	 * @throws JsonParseException
+	 *             from Jackson parser
+	 * @throws TimeoutException
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 * @throws URISyntaxException
 	 */
-	public HashMap<String, Object> checkClassifierStatus(String classifierID, String version) throws JsonParseException, JsonMappingException, IOException, InterruptedException, ExecutionException, TimeoutException, URISyntaxException {
-//		RequestSpecification request = RestAssured.given();
-//		request.basePath(getBaseAPI());
-//		request.baseUri(getBaseAPI());
-//		
-//		request.queryParam("api_key", getKey());
-//		request.queryParam("version", version);
-//		request.pathParam("classifier_id", classifierID);
-//		
-//		Response response = request.get("/v3/classifiers/{classifier_id}");
-//		
-//		if (response.getStatusCode() != 200)
-//			throw new RuntimeException("The service was unsuccessful in processing the request! HTTP: " + response.getStatusCode());
-		
+	public HashMap<String, Object> checkClassifierStatus(String classifierID, String version)
+			throws JsonParseException, JsonMappingException, IOException, InterruptedException, ExecutionException,
+			TimeoutException, URISyntaxException {
+		// RequestSpecification request = RestAssured.given();
+		// request.basePath(getBaseAPI());
+		// request.baseUri(getBaseAPI());
+		//
+		// request.queryParam("api_key", getKey());
+		// request.queryParam("version", version);
+		// request.pathParam("classifier_id", classifierID);
+		//
+		// Response response = request.get("/v3/classifiers/{classifier_id}");
+		//
+		// if (response.getStatusCode() != 200)
+		// throw new RuntimeException("The service was unsuccessful in
+		// processing the request! HTTP: " + response.getStatusCode());
+
 		URIBuilder builder = new URIBuilder();
-		builder.setScheme("https").setHost(getBaseAPI().replace("https://","")).setPath("/v3/classifiers/" + classifierID)
-			.setParameter("api_key", getKey())
-			.setParameter("version", version);
+		builder.setScheme("https").setHost(getBaseAPI().replace("https://", ""))
+				.setPath("/v3/classifiers/" + classifierID).setParameter("api_key", getKey())
+				.setParameter("version", version);
 		URI uri = builder.build();
-		
+
 		HttpGet get = new HttpGet(uri);
-		
-//		ContentResponse response = client.GET(uri);
+
+		// ContentResponse response = client.GET(uri);
 		HttpResponse response = client.execute(get);
 		if (response.getStatusLine().getStatusCode() != 200)
-			throw new RuntimeException("The service was unsuccessful in processing the request! HTTP: " + response.getStatusLine());
-		
-	    TypeReference<HashMap<String,Object>> typeRef = new TypeReference<HashMap<String,Object>>() {};
+			throw new RuntimeException(
+					"The service was unsuccessful in processing the request! HTTP: " + response.getStatusLine());
 
-//	    return mapper.readValue(response.getEntity().getContentAsString(), typeRef);
-	    BufferedReader rd = new BufferedReader(
-		        new InputStreamReader(response.getEntity().getContent()));
+		TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {
+		};
+
+		// return mapper.readValue(response.getEntity().getContentAsString(),
+		// typeRef);
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
 		StringBuffer result = new StringBuffer();
 		String line = "";
@@ -169,66 +197,72 @@ public class WatsonVRClient {
 		}
 		return mapper.readValue(result.toString(), typeRef);
 	}
-	
+
 	/**
-	 * Classifies the image using the REST call. This method retrieves the necessary parameters not from
-	 * parameters but from the file watson_properties.json. 
-	 * @param imageData String data representing the image bytes itself
-	 * @return a ClassifierOutput object which contains a list of scores for each class in each classifier
-	 * @throws IOException from Jackson parser
-	 * @throws JsonMappingException from Jackson parser
-	 * @throws JsonParseException from Jackson parser
-	 * @throws URISyntaxException 
+	 * Classifies the image using the REST call. This method retrieves the
+	 * necessary parameters not from parameters but from the file
+	 * watson_properties.json.
+	 * 
+	 * @param imageData
+	 *            String data representing the image bytes itself
+	 * @return a ClassifierOutput object which contains a list of scores for
+	 *         each class in each classifier
+	 * @throws IOException
+	 *             from Jackson parser
+	 * @throws JsonMappingException
+	 *             from Jackson parser
+	 * @throws JsonParseException
+	 *             from Jackson parser
+	 * @throws URISyntaxException
 	 */
-	public String classifyImage(String imageData) throws JsonParseException, JsonMappingException, IOException, URISyntaxException {
-//		RequestSpecification request = RestAssured.given();
-//		request.baseUri(getBaseAPI());
-//		request.basePath(getBaseAPI());
-//		
-//		request.queryParam("api_key", getKey());
-//		request.queryParam("classifier_ids", props.getClassifierID());
-//		request.queryParam("owners", props.getOwners());
-//		request.queryParam("threshold", props.getThreshold());
-//		request.queryParam("version", props.getVersion());
-//		
-//		request.formParam("images_file", imageData);
-		
+	public String classifyImage(String imageData)
+			throws JsonParseException, JsonMappingException, IOException, URISyntaxException {
+		// RequestSpecification request = RestAssured.given();
+		// request.baseUri(getBaseAPI());
+		// request.basePath(getBaseAPI());
+		//
+		// request.queryParam("api_key", getKey());
+		// request.queryParam("classifier_ids", props.getClassifierID());
+		// request.queryParam("owners", props.getOwners());
+		// request.queryParam("threshold", props.getThreshold());
+		// request.queryParam("version", props.getVersion());
+		//
+		// request.formParam("images_file", imageData);
+
 		try {
 			URIBuilder builder = new URIBuilder();
-			builder.setScheme("https").setHost(getBaseAPI().replace("https://","")).setPath("/v3/classify")
-				.setParameter("api_key", getKey())
-				.setParameter("classifier_ids", props.getClassifierID())
-				.setParameter("owners", props.getOwners())
-				.setParameter("threshold", "" + props.getThreshold())
-				.setParameter("version", props.getVersion());
+			builder.setScheme("https").setHost(getBaseAPI().replace("https://", "")).setPath("/v3/classify")
+					.setParameter("api_key", getKey()).setParameter("classifier_ids", props.getClassifierID())
+					.setParameter("owners", props.getOwners()).setParameter("threshold", "" + props.getThreshold())
+					.setParameter("version", props.getVersion());
 			URI uri = builder.build();
-			
-//		Response response = request.post("/v3/classify");
-			
+
+			// Response response = request.post("/v3/classify");
+
 			HttpPost post = new HttpPost(uri);
-			
+
 			MultipartEntityBuilder me = MultipartEntityBuilder.create();
 			me.addPart("images_file", new FileBody(new File(imageData)));
 			post.setEntity(me.build());
-			
+
 			HttpResponse response = client.execute(post);
-			
+
 			if (response.getStatusLine().getStatusCode() != 200)
-				throw new RuntimeException("The service was unsuccessful in processing the request! HTTP: " + response.getStatusLine());
-			
-			BufferedReader rd = new BufferedReader(
-			        new InputStreamReader(response.getEntity().getContent()));
+				throw new RuntimeException(
+						"The service was unsuccessful in processing the request! HTTP: " + response.getStatusLine());
+
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
 			StringBuffer result = new StringBuffer();
 			String line = "";
 			while ((line = rd.readLine()) != null) {
 				result.append(line);
 			}
-			
+
 			return ResponseParser.decide(result.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "An error occurred while the system was accessing Watson's API. Please try again later."; 
+			return "An error occurred while the system was accessing Watson's API. Please try again later.";
 		}
 	}
 }
